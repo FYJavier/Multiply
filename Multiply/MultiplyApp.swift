d5d08bf93d91ae299e66cbf6a86595c49b34870b
Freya Javier <thesleepyunicorn63@gmail.com>
2022-07-21 14:54:31 -0500

    Initial Commit

---
+++ Multiply/MultiplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplyApp.swift
+//  Multiply
+//
+//  Created by Yuna on 7/19/22.
+//
+
+import SwiftUI
+
+@main
+struct MultiplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
